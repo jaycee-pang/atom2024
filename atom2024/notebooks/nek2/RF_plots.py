330302569f1d1a6fc060a7f3a7ccac7469590473
Jaycee Pang <jayceepang@berkeley.edu>
2024-03-14 05:08:16 -0700

    added roc auc vs params plots

---
+++ atom2024/notebooks/nek2/RF_plots.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, precision_score, f1_score, roc_auc_score, roc_curve, precision_recall_curve, auc, recall_score
+import math
+import torch
+import numpy as np
+# import gpytorch
+import pandas as pd
+import seaborn as sns
+import os
+import pickle
+import shutil
+import matplotlib 
+from matplotlib import pyplot as plt
+from RF_GSCV import *
+
+
+    
+
